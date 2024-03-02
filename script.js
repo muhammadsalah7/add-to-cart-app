490139a8587008d6e0e40f37d18250577d23d9f2
Muhammad Salah <muhammadsalah217@gmail.com>
2024-03-02 09:54:52 +0200

    created the RTDB

---
+++ script.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+const appSettings = {
+  databaseURL:
+    "https://playground-99fa1-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const addBtn = document.getElementById("add-button");
+const inputField = document.getElementById("input-field");
+
+addBtn.addEventListener("click", () => {
+  let inputValue = inputField.value;
+  push(shoppingListInDB, inputValue);
+  console.log(inputValue);
+});
